066127cc4719cf342c5166295e04a80156c4048f
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-05-26 23:45:03 +0300

    In KidsRepository.kt, inject Scope + Dispatcher

--- app/src/main/java/com/vladislaviliev/birthday/kids/KidsRepository.kt
+++ app/src/main/java/com/vladislaviliev/birthday/kids/KidsRepository.kt
@@ -1,13 +1,19 @@
 package com.vladislaviliev.birthday.kids
 
 import com.vladislaviliev.birthday.networking.State
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
-class KidsRepository(private val api: KidsApi) {
-
+class KidsRepository(
+    private val scope: CoroutineScope,
+    private val dispatcher: CoroutineDispatcher,
+    private val api: KidsApi,
+) {
     val state: Flow<State> = api.state
 
-    suspend fun connect() {
+    suspend fun connect() = scope.launch(dispatcher) {
         api.connect()
-    }
+    }.join()
 }
--- app/src/test/java/com/vladislaviliev/birthday/KidsRepositoryTest.kt
+++ app/src/test/java/com/vladislaviliev/birthday/KidsRepositoryTest.kt
@@ -1,26 +1,32 @@
 package com.vladislaviliev.birthday
 
 import com.vladislaviliev.birthday.kids.InMemoryKidsApi
+import com.vladislaviliev.birthday.kids.KidsApi
 import com.vladislaviliev.birthday.kids.KidsRepository
 import com.vladislaviliev.birthday.networking.Response
 import com.vladislaviliev.birthday.networking.State
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.LocalDateTime
 import org.junit.Assert
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class KidsRepositoryTest {
+
+    fun TestScope.createRepo(api: KidsApi) = KidsRepository(this, coroutineContext[CoroutineDispatcher]!!, api)
 
     @Test
     fun `repository should have disconnected initial state`() = runTest {
-        val repository = KidsRepository(InMemoryKidsApi())
-        Assert.assertEquals(State.Disconnected(), repository.state.first())
+        Assert.assertEquals(State.Disconnected(), createRepo(InMemoryKidsApi()).state.first())
     }
 
     @Test
     fun `connect should change state to connected`() = runTest {
-        val repository = KidsRepository(InMemoryKidsApi())
+        val repository = createRepo(InMemoryKidsApi())
         repository.connect()
         Assert.assertEquals(State.Connected(), repository.state.first())
     }
@@ -28,11 +34,15 @@ class KidsRepositoryTest {
     @Test
     fun `repository should reflect api response state changes`() = runTest {
         val api = InMemoryKidsApi()
-        val repository = KidsRepository(api)
+        val repository = createRepo(api)
 
         val response = Response("JohnyDoe", LocalDateTime.parse("2025-05-26T00:19:16"), Theme.PELICAN)
         api.emitResponse(response)
 
-        Assert.assertEquals(State.Connected(response), repository.state.first())
+        val state = repository.state.first()
+
+        println(api.state.first())
+
+        Assert.assertEquals(State.Connected(response), state)
     }
 }
