99ddebcbcb9e1069f2a8b5d5e5200466962e766c
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-05-30 05:45:17 +0300

    Gallery picker uses the built-in picker

--- app/src/main/java/com/vladislaviliev/birthday/avatarPicker/gallery/GalleryPicker.kt
+++ app/src/main/java/com/vladislaviliev/birthday/avatarPicker/gallery/GalleryPicker.kt
@@ -1,6 +1,11 @@
 package com.vladislaviliev.birthday.avatarPicker.gallery
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.dialog
@@ -9,13 +14,16 @@ import kotlinx.serialization.Serializable
 @Serializable
 object GalleryPickerRoute
 
-fun NavGraphBuilder.addGalleryPickerDestination(onImageSelected: () -> Unit) {
+fun NavGraphBuilder.addGalleryPickerDestination(onImageSelected: (Uri) -> Unit) {
     dialog<GalleryPickerRoute> { Content(onImageSelected) }
 }
 
 @Composable
-fun Content(onImageSelected: () -> Unit) {
-
+fun Content(onImageSelected: (Uri) -> Unit) {
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+        onImageSelected(it ?: Uri.EMPTY)
+    }
+    LaunchedEffect(true) { launcher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }
 }
 
 fun NavController.navigateToGalleryPicker() {
@@ -23,6 +31,6 @@ fun NavController.navigateToGalleryPicker() {
     navigate(GalleryPickerRoute)
 }
 
-fun NavController.onImageSelected() {
+fun NavController.onImageSelected(uri: Uri) {
     popBackStack()
 }
