3eab58a0d794efc0911caad6518df6205be74566
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-05-30 12:46:56 +0300

    In ActivityViewModel expose the bitmap state flow eagerly - load ASAP

--- app/src/main/java/com/vladislaviliev/birthday/ActivityViewModel.kt
+++ app/src/main/java/com/vladislaviliev/birthday/ActivityViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.vladislaviliev.birthday.avatarPicker.AvatarRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.io.File
 import javax.inject.Inject
@@ -12,7 +14,7 @@ import javax.inject.Inject
 @HiltViewModel
 class ActivityViewModel @Inject constructor(private val avatarRepository: AvatarRepository) : ViewModel() {
 
-    val bitmap = avatarRepository.bitmap
+    val bitmap = avatarRepository.bitmap.stateIn(viewModelScope, SharingStarted.Eagerly, null)
 
     fun saveAvatarFromUri(uri: Uri) {
         viewModelScope.launch { avatarRepository.saveFromUri(uri) }
