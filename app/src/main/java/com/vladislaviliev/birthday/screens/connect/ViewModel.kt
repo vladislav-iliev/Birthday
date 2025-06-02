6049067732d7e93ea9dea6af6ecb12c8622f220b
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-06-02 11:23:04 +0300

    Add early instantiation of kid/avatar/Repository, kid/text/Repository

--- app/src/main/java/com/vladislaviliev/birthday/screens/connect/ViewModel.kt
+++ app/src/main/java/com/vladislaviliev/birthday/screens/connect/ViewModel.kt
@@ -2,7 +2,6 @@ package com.vladislaviliev.birthday.screens.connect
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.vladislaviliev.birthday.kid.text.Repository
 import com.vladislaviliev.birthday.networking.Api
 import com.vladislaviliev.birthday.networking.NetworkState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,11 +9,14 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import com.vladislaviliev.birthday.kid.avatar.Repository as AvatarRepository
+import com.vladislaviliev.birthday.kid.text.Repository as TextRepository
 
 @HiltViewModel
 class ViewModel @Inject constructor(
-    textRepository: Repository, // To trigger early instantiation. Don't remove!
-    private val api: Api
+    private val api: Api,
+    textRepository: TextRepository, // To trigger early instantiation for App Dependency Container. Don't remove!
+    avatarRepository: AvatarRepository // To trigger early instantiation for App Dependency Container. Don't remove!
 ) : ViewModel() {
 
     val networkState =
