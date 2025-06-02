a180a0f28cfe686158f4e28ee0e149174d5199c3
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-06-02 13:46:04 +0300

    Remove collecting of kid/State in screens/kid/ViewModel

--- app/src/main/java/com/vladislaviliev/birthday/screens/kid/ViewModel.kt
+++ app/src/main/java/com/vladislaviliev/birthday/screens/kid/ViewModel.kt
@@ -1,15 +1,12 @@
 package com.vladislaviliev.birthday.screens.kid
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.vladislaviliev.birthday.kid.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ViewModel @Inject constructor(repository: Repository) : ViewModel() {
 
-    val state = repository.state.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), repository.state.value)
+    val state = repository.state
 }
